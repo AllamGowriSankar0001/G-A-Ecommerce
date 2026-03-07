da805901d21fad326c7db78927a0f3171cc61e62
AllamGowriSankar0001 <allamgowrisankar0001@gmail.com>
2026-03-07 21:55:09 +0530

    fixing error iin nodemailer brevo try2

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -4,10 +4,12 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
+  family: 4, // force IPv4
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD
-  }
+  },
+  connectionTimeout: 10000
 });
 
 module.exports = transporter;
